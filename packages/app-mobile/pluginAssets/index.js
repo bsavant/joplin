c5e8ab85f76238c0395b51972898d3bdc031af20
bsavant <209364+bsavant@users.noreply.github.com>
2025-04-23 20:36:16 -0400

    Upgrade mermaid to 11.6.0

--- packages/app-mobile/pluginAssets/index.js
+++ packages/app-mobile/pluginAssets/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-hash:"bccea4c7bed254bfbfa0fcf40f3f5cdf", files: {
+hash:"bfd3de199d980b33b9059486c08baafb", files: {
 'highlight.js/atom-one-dark-reasonable.css': { data: require('./highlight.js/atom-one-dark-reasonable.css.base64.js'), mime: 'text/css', encoding: 'base64' },
 'highlight.js/atom-one-light.css': { data: require('./highlight.js/atom-one-light.css.base64.js'), mime: 'text/css', encoding: 'base64' },
 'katex/fonts/KaTeX_AMS-Regular.woff2': { data: require('./katex/fonts/KaTeX_AMS-Regular.woff2.base64.js'), mime: 'application/octet-stream', encoding: 'base64' },
